f2153259d2634042b3efda3234e394de8bf9cbd1
jumpsplat120 <jumpsplat120@yahoo.com>
2022-10-13 17:02:24 -0700

    Work at EPS
    Template issues: Page 1 debtor/creditor fields not filling
    page 2 debtor name field not filling
    page 11 garnisher name not filling and size incorrect
    All "Circuits" should be center justified
    Debtor field on last page not filling
    Creditor smush is triggering with only one plaintiff

--- main.js
+++ main.js
@@ -116,6 +116,21 @@ court_counties.washington = new County("Washington", "Hillsboro", 97124, "145 NE
 court_counties.wheeler = new County("Wheeler", "Fossil", 97830, "701 Adams St", "Oregon", "#204");
 court_counties.yamhill = new County("Yamhill", "McMinnville", 97128, "535 NE 5th St", "Oregon");
 
+//wait for a func to return true
+async function waitFor(func) {
+    function wrapper(done) {
+        if (func()) {
+            done();
+        } else {
+            setTimeout(wrapper, 0, done);
+        }
+    }
+
+    return new Promise(done => {
+        setTimeout(wrapper, 0, done);
+    });
+}
+
 //Function to filter which buttons should show when
 //typing in court dropdown
 function filterDropdown() {
@@ -169,6 +184,24 @@ function getValue(id) {
 }
 
 async function main() {
+    const data = {};
+
+    data.money_maker = [];
+    data.money_loser = [];
+
+    data.attorney   = {};
+    data.main_loser = {};
+    data.main_maker = {};
+    data.poe        = {};
+    data.fee        = {};
+    data.files      = {};
+    data.date       = {};
+
+    data.fee.interest = {};
+    data.fee.delivery = {};
+    data.fee.issuance = {};
+    data.fee.search   = {};
+
     //Add event to add text to input when clicking button dropdown
     for (const button of eid("dropdown_content").getElementsByTagName("button")) {
         button.addEventListener("click", e => {
@@ -204,27 +237,25 @@ async function main() {
         if (e.target.getAttribute("data-showing") == 3) eid(`money_${type}_c`).style.display = "block";
     }, "add_money_maker", "add_money_maker");
 
+    //template and signature loads files into data.
+    listen("change", e => {
+        const reader = new FileReader();
+        const name   = e.target.id;
+        
+        reader.addEventListener("load", async (e) => { data.files[name] = e.target.result; });
+
+        reader.readAsArrayBuffer(e.target.files[0]);
+    }, "signature", "template");
+
     //Clicking save button saves pdf to desktop
     listen("click", async (e) => {
-        const raw  = await fetch('/template.pdf');
-        const buf  = await raw.arrayBuffer();
-        const doc  = await PDFDocument.load(buf);
+        await waitFor(_ => data.files.template);
+        const doc  = await PDFDocument.load(data.files.template);
         const font = await doc.embedFont(StandardFonts.TimesRoman);
         const form = doc.getForm();
-
-        const data = {};
         
-        let count, location;
-
-        data.money_maker = [];
-        data.money_loser = [];
-
-        data.attorney   = {};
-        data.main_loser = {};
-        data.main_maker = {};
-        data.poe        = {};
-        data.fees       = {};
-
+        let subtotal, str_num, location;
+        
         data.attorney.name   = "William D. Schaub, P.C."
         data.attorney.city   = "";
         data.attorney.state  = "";
@@ -246,7 +277,7 @@ async function main() {
         data.money_loser[0] = getValue("money_loser_a");
         data.money_loser[1] = getValue("money_loser_b");
         data.money_loser[2] = getValue("money_loser_c");
-        //data.amount_owed    = getValue("amount_owed");
+        //data.amount_owed  = getValue("amount_owed");
         data.main_maker.street = getValue("main_maker_address_street");
         data.main_maker.zip    = getValue("main_maker_address_zip");
         data.main_maker.misc   = getValue("main_maker_address_misc");
@@ -258,25 +289,25 @@ async function main() {
         data.poe.street = getValue("poe_address_street");
         data.poe.zip    = getValue("poe_address_zip");
         data.poe.misc   = getValue("poe_address_misc");
-        data.fee.original_debt = getValue("debt_original");
-        data.fee.less_payments = getValue("less_payments");
-        data.fee.transcript = getValue("transcript_fee");
-        data.fee.attorney   = getValue("attorney_fee");
-        data.fee.cost       = getValue("cost_fee");
-        data.fee.sheriff    = getValue("sheriff_fee");
-        data.fee.party      = getValue("party_fee");
-        data.fee.research   = getValue("research_fee");
-        data.fee.other      = getValue("other_fee");
-        data.fee.interest.pre      = getValue("pre_interest");
-        data.fee.interest.post     = getValue("post_interest");
-        data.fee.delivery.current  = getValue("delivery_fee_current");
-        data.fee.issuance.current  = getValue("issuance_fee_current");
-        data.fee.search.current    = getValue("search_fee_current");
-        data.fee.issuance.previous = getValue("issuance_fee_previous");
-        data.fee.delivery.previous = getValue("delivery_fee_previous");
-        data.fee.search.previous   = getValue("search_fee_previous");
-        data.date.calculation = getValue("calculation_date");
-        data.date.judgement   = getValue("judgement_date");
+        data.fee.original_debt = Number(getValue("debt_original") || 0);
+        data.fee.less_payments = Number(getValue("less_payments") || 0);
+        data.fee.transcript = Number(getValue("transcript_fee") || 0);
+        data.fee.attorney   = Number(getValue("attorney_fee") || 0);
+        data.fee.cost       = Number(getValue("cost_fee") || 0);
+        data.fee.sheriff    = Number(getValue("sheriff_fee") || 0);
+        data.fee.party      = Number(getValue("party_fee") || 0);
+        data.fee.research   = Number(getValue("research_fee") || 0);
+        data.fee.other      = Number(getValue("other_fee") || 0);
+        data.fee.interest.pre      = Number(getValue("pre_interest") || 0);
+        data.fee.interest.post     = Number(getValue("post_interest") || 0);
+        data.fee.delivery.current  = Number(getValue("delivery_fee_current") || 0);
+        data.fee.issuance.current  = Number(getValue("issuance_fee_current") || 0);
+        data.fee.search.current    = Number(getValue("search_fee_current") || 0);
+        data.fee.issuance.previous = Number(getValue("issuance_fee_previous") || 0);
+        data.fee.delivery.previous = Number(getValue("delivery_fee_previous") || 0);
+        data.fee.search.previous   = Number(getValue("search_fee_previous") || 0);
+        data.date.calculation = new Date(getValue("calculation_date"));
+        data.date.judgement   = new Date(getValue("judgement_date"));
         data.branch_number    = getValue("branch_number");
 
         data.main_maker.name = data.money_maker.length == 1 ? data.money_maker[0] : getValue("main_maker_name");
@@ -284,21 +315,21 @@ async function main() {
 
         location = zips_database[data.main_loser.zip];
 
-        data.main_loser.city   = location.city;
-        data.main_loser.county = location.county_name;
-        data.main_loser.state  = location.state_name;
+        data.main_loser.city   = location?.city.toUpperCase();
+        data.main_loser.county = location?.county_name.toUpperCase();
+        data.main_loser.state  = location?.state_name.toUpperCase();
 
         location = zips_database[data.main_maker.zip];
 
-        data.main_maker.city   = location.city;
-        data.main_maker.county = location.county_name;
-        data.main_maker.state  = location.state_name;
+        data.main_maker.city   = location?.city.toUpperCase();
+        data.main_maker.county = location?.county_name.toUpperCase();
+        data.main_maker.state  = location?.state_name.toUpperCase();
 
         location = zips_database[data.poe.zip];
 
-        data.poe.city   = location.city;
-        data.poe.county = location.county_name;
-        data.poe.state  = location.state_name;
+        data.poe.city   = location?.city.toUpperCase();
+        data.poe.county = location?.county_name.toUpperCase();
+        data.poe.state  = location?.state_name.toUpperCase();
 
         form.getField("money_loser_a").setText(data.money_loser[0]);
         form.getField("money_loser_b").setText(data.money_loser[1]);
@@ -315,7 +346,7 @@ async function main() {
         if (data.money_maker.length == 1) {
             form.getField("money_maker_name_scrunch").setText(data.money_maker[0]);
         } else {
-            form.getField("money_maker_name_scrunch").setText(data.money_maker[0] + data.money_maker[1] + data.money_maker[2]);
+            form.getField("money_maker_name_scrunch").setText(`${data.money_maker[0]}, ${data.money_maker[1]}, ${data.money_maker[2]}`);
         }
 
         const county = court_counties[data.court_county.toLowerCase()];
@@ -332,10 +363,11 @@ async function main() {
         form.getField("poe_address_misc").setText(data.poe.misc);
 
         form.getField("main_loser_name").setText(data.main_loser.name);
-        form.getField("main_loser_last_four_ssn").setText(data.main_loser.ssn);
+        form.getField("main_loser_last_four_ssn").setText(`${data.main_loser.ssn.slice(0, 4)}-${data.main_loser.ssn.slice(4,6)}-${data.main_loser.ssn.slice(6,10)}`);
 
         if (!(data.main_loser.street && data.main_loser.city && data.main_loser.state && data.main_loser.zip)) {
             form.getField("main_loser_address_missing").check();
+            form.getField("main_loser_address_street").setText("UNKNOWN");
         } else {
             form.getField("main_loser_address_street").setText(data.main_loser.street);
             form.getField("main_loser_address_city").setText(data.main_loser.city);
@@ -353,59 +385,59 @@ async function main() {
 
         form.getField("case_number").setText(data.case_number);
         form.getField("account_number").setText(data.account_number);
-        form.getField("amount_owed").setText(data.amount_owed);
 
-        form.getField("judgement_date_day_month").setText(`${data.judgement_date.getUTCMonth() + 1}/${data.judgement_date.getUTCDate()}`);
-        form.getField("judgement_date_year").setText(`${data.judgement_date.getUTCFullYear()}`);
+        form.getField("judgement_date_day_month").setText(`${data.date.judgement.getUTCMonth() + 1}/${data.date.judgement.getUTCDate()}`);
+        form.getField("judgement_date_year").setText(`${data.date.judgement.getUTCFullYear()}`);
         
-        form.getField("debt_original").setText(data.debt_original == 0 ? "" : data.debt_original.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("pre_interest").setText(data.pre_interest == 0 ? "" : data.pre_interest.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("attorney_fee").setText(data.attorney_fee == 0 ? "" : data.attorney_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("cost_fee").setText(data.cost_fee == 0 ? "" : data.cost_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("post_interest").setText(data.post_interest == 0 ? "" : data.post_interest.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("search_fee_current").setText(data.search_fee_current == 0 ? "" : data.search_fee_current.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("search_fee_previous").setText(data.search_fee_previous == 0 ? "" : data.search_fee_previous.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("sheriff_fee").setText(data.sheriff_fee == 0 ? "" : data.sheriff_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("party_free").setText(data.party_fee == 0 ? "" : data.party_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("research_fee").setText(data.research_fee == 0 ? "" : data.research_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("other_fee").setText(data.other_fee == 0 ? "" : data.other_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("issuance_fee_current").setText(data.issuance_fee_current == 0 ? "" : data.issuance_fee_current.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("delivery_fee_current").setText(data.delivery_fee_current == 0 ? "" : data.delivery_fee_current.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("delivery_fee_previous").setText(data.delivery_fee_previous == 0 ? "" : data.delivery_fee_previous.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("issuance_fee_previous").setText(data.issuance_fee_previous == 0 ? "" : data.issuance_fee_previous.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("transcript_fee").setText(data.transcript_fee == 0 ? "" : data.transcript_fee.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("debt_original").setText(data.fee.original_debt == 0 ? "" : data.fee.original_debt.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("pre_interest").setText(data.fee.interest.pre == 0 ? "" : data.fee.interest.pre.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("attorney_fee").setText(data.fee.attorney == 0 ? "" : data.fee.attorney.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("cost_fee").setText(data.fee.cost == 0 ? "" : data.fee.cost.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("post_interest").setText(data.fee.interest.post == 0 ? "" : data.fee.interest.post.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("search_fee_current").setText(data.fee.search.current == 0 ? "" : data.fee.search.current.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("search_fee_previous").setText(data.fee.search.previous == 0 ? "" : data.fee.search.previous.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("sheriff_fee").setText(data.fee.sheriff == 0 ? "" : data.fee.sheriff.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("party_free").setText(data.fee.party == 0 ? "" : data.fee.party.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("research_fee").setText(data.fee.research == 0 ? "" : data.fee.research.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("other_fee").setText(data.fee.other == 0 ? "" : data.fee.other.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("issuance_fee_current").setText(data.fee.issuance.current   == 0 ? "" : data.fee.issuance.current.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("delivery_fee_current").setText(data.fee.delivery.current   == 0 ? "" : data.fee.delivery.current.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("delivery_fee_previous").setText(data.fee.delivery.previous == 0 ? "" : data.fee.delivery.previous.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("issuance_fee_previous").setText(data.fee.issuance.previous == 0 ? "" : data.fee.issuance.previous.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("transcript_fee").setText(data.fee.transcript == 0 ? "" : data.fee.transcript.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
 
-        let subtotal  = data.debt_original
-            subtotal += data.pre_interest
-            subtotal += data.attorney_fee
-            subtotal += data.cost_fee
-            subtotal += data.post_interest
-            subtotal += data.search_fee_current
-            subtotal += data.search_fee_previous
-            subtotal += data.sheriff_fee 
-            subtotal += data.party_fee
-            subtotal += data.research_fee
-            subtotal += data.other_fee
-            subtotal += data.issuance_fee_current
-            subtotal += data.delivery_fee_current
-            subtotal += data.issuance_fee_previous 
-            subtotal += data.transcript_fee
+        subtotal  = data.fee.original_debt;
+        subtotal += data.fee.transcript;
+        subtotal += data.fee.attorney;
+        subtotal += data.fee.cost;
+        subtotal += data.fee.sheriff;
+        subtotal += data.fee.party;
+        subtotal += data.fee.research;
+        subtotal += data.fee.other;
+        subtotal += data.fee.interest.pre;
+        subtotal += data.fee.interest.post;
+        subtotal += data.fee.delivery.current;
+        subtotal += data.fee.issuance.current;
+        subtotal += data.fee.search.current;
+        subtotal += data.fee.issuance.previous;
+        subtotal += data.fee.delivery.previous;
+        subtotal += data.fee.search.previous;
+
+        str_num = data.branch_number?.toString() || "";
 
         form.getField("subtotal").setText(subtotal.toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("less_payments").setText((data.less_payments || 0).toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        form.getField("total_amount_owed").setText((subtotal - Number(data.less_payments || "0")).toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
-        let str_num = data.branch_number.toString();
+        form.getField("less_payments").setText((data.fee.less_payments || 0).toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
+        form.getField("total_amount_owed").setText((subtotal - Number(data.fee.less_payments || "0")).toLocaleString("en-us",{useGrouping:true, minimumFractionDigits:2}));
         form.getField("branch_number").setText(`(${str_num.slice(0, 3)}) ${str_num.slice(3,6)}-${str_num.slice(6,10)}`);
 
         const date = new Date();
 
         form.getField("todays_date").setText(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
-        form.getField("calculation_date_year").setText(`${(""+data.calculation_date.getUTCFullYear()).slice(1)}`);
-        form.getField("calculation_date_day_month").setText(`${data.calculation_date.getUTCMonth() + 1}/${data.calculation_date.getUTCDate()}`);
+        form.getField("calculation_date_year").setText(`${(""+data.date.calculation.getUTCFullYear()).slice(1)}`);
+        form.getField("calculation_date_day_month").setText(`${data.date.calculation.getUTCMonth() + 1}/${data.date.calculation.getUTCDate()}`);
         
-        const sig_fetch = await fetch("signature.png");
-        const sig_bytes = await sig_fetch.arrayBuffer();
-        const sig = await doc.embedPng(sig_bytes);
+        await waitFor(_=> data.files.signature);
+        const sig = await doc.embedPng(data.files.signature);
 
         form.getField("signature_bounds").setImage(sig);
 
